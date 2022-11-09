39de05428ae746a3732824ca3f6ae6956d4b719b
kiran <kirven05@gmail.com>
2022-11-09 11:23:59 +0530

    brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { google, slack, dropbox, shopify } from './imports';
-import './brand.css';
+import React from "react";
+import { google, slack, dropbox, shopify } from "./imports";
+import "./brand.css";
 
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA"></div>
-    <div>
-      <img src={google} />
+      <div>
+        <img src={google} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
     </div>
-    <div>
-      <img src={slack} />
-    </div>
-    <div>
-      <img src={dropbox} />
-    </div>
-    <div>
-      <img src={shopify} />
-    </div>
-  <div className="gpt3__blog-heading">
-  <h1 className="gradient__text">Key Note Speakers</h1> <br/> <br/>
-  </div>
-  </div>
+      <div className="gpt3__blog-heading">
+        <h1>Key Note Speakers</h1> <br /> <br />
+      </div>
   </div>
 );
 
