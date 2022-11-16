4f8445f783b8aef9851966ce0973d29f6497e017
amruthkiran <amruthkiran47@gmail.com>
2022-11-17 01:40:36 +0530

    added form

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { google, slack, dropbox, shopify } from "./imports";
 import "./brand.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 const Brand = () => (
-  <div className="gpt3__brand section__padding">
-    <div className="gpt3__blog-container">
-      <div className="gpt3__blog-container_groupA"></div>
-      <div>
+  <div className="gpt3__brand section__padding" data-aos="zoom-in-up">
+    <div className="gpt3__blog-heading">
+      <h1>Key Note Speakers</h1>
+    </div>
+    <div className="gpt3__blog-container" >
+      {/* <div className="gpt3__blog-container_groupA"></div> */}
+      <div data-aos="fade-up">
         <img src={google} />
       </div>
-      <div>
+      <div data-aos="fade-up">
         <img src={slack} />
       </div>
-      <div>
+      <div data-aos="fade-up">
         <img src={dropbox} />
       </div>
-      <div>
+      <div data-aos="fade-up">
         <img src={shopify} />
       </div>
-    </div>
-    <div className="gpt3__blog-heading">
-      <h1>Key Note Speakers</h1> <br /> <br />
     </div>
   </div>
 );
